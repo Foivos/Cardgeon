358806abce4a9afa2ef7de86849c8a3a00e8128a
Foivos <foivos@umich.edu>
2020-11-11 18:48:30 +0200

    Added an effects file.

---
+++ src/js/effects/Effects.js
@@ -0,0 +1,22 @@
+
+export const targets = {
+    squares : 'squares',
+    creatures : 'creatures',
+};
+
+export const effects = {
+    damage : {
+        targets : 'creatures',
+        effect : damage,
+    },
+    armor : {
+        targets : 'creatures',
+        effect : armor,
+    }
+}
+
+function damage () {
+}
+
+function armor() {
+}
